dc8945dd804ba76a823b00c741ee9d208f4ac18d
SVEK <svek.95@gmail.com>
2019-03-19 10:20:21 +0100

    rename and add 2lab

--- src/main/java/Lab2/App.java
+++ src/main/java/Lab2/App.java
@@ -1,13 +1,12 @@
+package Lab2;
 
-
-
-import Lab1.CarWebService;
+//import Lab1.CarWebService;
 
 import javax.xml.ws.Endpoint;
 
 public class App {
     public static void main(String[] args) {
         String url = "http://localhost:8081/CarService";
-        Endpoint.publish(url, new CarWebService());
+        Endpoint.publish(url, new CarWebCrudService());
     }
 }
