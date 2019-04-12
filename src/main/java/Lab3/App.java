fc255e24f2a54706fae9c94e7a8617dbebc5af6c
SVEK <svek.95@gmail.com>
2019-04-12 15:07:05 +0200

    Add Lab3 Start

---
+++ src/main/java/Lab3/App.java
@@ -0,0 +1,11 @@
+package Lab3;
+
+import javax.xml.ws.Endpoint;
+
+public class App {
+    public static void main(String[] args) {
+        System.setProperty("com.sun.xml.ws.fault.SOAPFaultBuilder.disableCaptureStackTrace", "false");
+        String url = "http://0.0.0.0:8081/CarService";
+        Endpoint.publish(url, new CarWebExcept());
+    }
+}
